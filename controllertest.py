9a8f232ae070e602b8d29b2a92d0159838035217
Binary-Ninja <91568729+Binary-Ninja@users.noreply.github.com>
2024-04-29 12:11:37 -0600

    Add files via upload

--- controllertest.py
+++ controllertest.py
@@ -24,15 +24,44 @@ pg.init()
 pgc.init()
 
 
+AXIS_NAMES = {
+    pg.CONTROLLER_AXIS_LEFTX: "Left_X",
+    pg.CONTROLLER_AXIS_LEFTY: "Left_Y",
+    pg.CONTROLLER_AXIS_RIGHTX: "Right_X",
+    pg.CONTROLLER_AXIS_RIGHTY: "Right_Y",
+    pg.CONTROLLER_AXIS_TRIGGERLEFT: "L_Trigger",
+    pg.CONTROLLER_AXIS_TRIGGERRIGHT: "R_Trigger",
+}
+
+BUTTON_NAMES = {
+    pg.CONTROLLER_BUTTON_A: "A",
+    pg.CONTROLLER_BUTTON_B: "B",
+    pg.CONTROLLER_BUTTON_X: "X",
+    pg.CONTROLLER_BUTTON_Y: "Y",
+    pg.CONTROLLER_BUTTON_BACK: "BACK",
+    pg.CONTROLLER_BUTTON_GUIDE: "GUIDE",
+    pg.CONTROLLER_BUTTON_START: "START",
+    pg.CONTROLLER_BUTTON_LEFTSTICK: "L_STICK",
+    pg.CONTROLLER_BUTTON_RIGHTSTICK: "R_STICK",
+    pg.CONTROLLER_BUTTON_LEFTSHOULDER: "L_SHOULDER",
+    pg.CONTROLLER_BUTTON_RIGHTSHOULDER: "R_SHOULDER",
+    pg.CONTROLLER_BUTTON_DPAD_UP: "UP",
+    pg.CONTROLLER_BUTTON_DPAD_DOWN: "DOWN",
+    pg.CONTROLLER_BUTTON_DPAD_LEFT: "LEFT",
+    pg.CONTROLLER_BUTTON_DPAD_RIGHT: "RIGHT",
+}
+
+
 def main():
     logger.info("Program started.")
     pg.display.set_caption("Controller Testing")
     screen = pg.display.set_mode((500, 500))
     clock = pg.time.Clock()
     font = pg.Font(None, 25)
-    lines: list[str] = []
+
     controllers: dict[int, pgc.Controller] = {}
     rumbles: dict[int, bool] = {}
+    display_index: int = 0
 
     while True:
         for event in pg.event.get():
@@ -45,75 +74,87 @@ def main():
                     logger.info("Program ended.")
                     pg.quit()
                     sys.exit()
+                if event.key in (pg.K_RIGHT, pg.K_UP):
+                    display_index += 1
+                    display_index %= len(controllers)
+                if event.key in (pg.K_LEFT, pg.K_RIGHT):
+                    display_index -= 1
+                    display_index %= len(controllers)
 
             if event.type == pg.JOYDEVICEADDED:
                 if pgc.is_controller(event.device_index):
-                    logger.debug("Joy added (Controller).")
+                    logger.info(f"Joystick ID-{event.device_index} added (Controller).")
                 else:
-                    logger.debug("Joy added (Joystick).")
-                    logger.info("Attempting to convert bad Joystick.")
+                    logger.info(f"Joystick ID-{event.device_index} added (Joystick).")
+                    logger.info("Attempting to convert bad Joystick...")
                     try:
-                        pgc.Controller.from_joystick(pg.Joystick(event.device_index))
+                        c = pgc.Controller.from_joystick(pg.Joystick(event.device_index))
+                        rumble = c.rumble(0.7, 0.7, 500)
+                        logger.info(f"Converted ID-{c.id} \"{c.name}\". Rumble: {rumble}")
+                        logger.info(f"Mapping: {c.get_mapping()}")
                     except Exception as ex:
-                        logger.info(f"Nope: {ex}")
+                        logger.warning(f"Error {ex.__class__}: {ex}")
+
             if event.type == pg.JOYDEVICEREMOVED:
-                logger.debug("Joy removed.")
+                logger.info(f"Joystick ID-{event.instance_id} removed.")
 
             if event.type == pg.CONTROLLERDEVICEADDED:
-                c = pgc.Controller(event.device_index)
+                try:
+                    c = pgc.Controller(event.device_index)
+                except Exception as ex:
+                    logger.warning(f"Controller ID-{event.device_index} could not be initialized.")
+                    logger.warning(f"Error {ex.__class__}: {ex}")
+                    continue
                 controllers[c.id] = c
-                if c.rumble(0, 0.7, 100):
-                    rumbles[c.id] = True
-                else:
-                    rumbles[c.id] = False
-                logger.info(f"Controller {c.id} {c.name} added. Rumble: {rumbles[c.id]}")
+                rumbles[c.id] = c.rumble(0.7, 0.7, 500)
+                logger.info(f"Controller ID-{c.id} \"{c.name}\" added. Rumble: {rumbles[c.id]}")
                 logger.info(f"Mapping: {c.get_mapping()}")
+
             if event.type == pg.CONTROLLERDEVICEREMOVED:
                 if event.instance_id in controllers:
                     c = controllers[event.instance_id]
-                    logger.info(f"Controller {c.id} {c.name} removed.")
+                    logger.info(f"Controller ID-{c.id} \"{c.name}\" removed.")
                     del controllers[c.id]
                     del rumbles[c.id]
+                    if len(controllers):
+                        display_index %= len(controllers)
+                    else:
+                        display_index = 0
                 else:
-                    logger.info("Controller ??? removed.")
+                    logger.info("Controller ID-?? \"UNKNOWN\" removed.")
+
             if event.type == pg.CONTROLLERDEVICEREMAPPED:
                 c = controllers[event.instance_id]
                 logger.info(f"Controller {c.id} {c.name} remapped.")
-
-            if event.type in (pg.JOYBUTTONUP, pg.JOYBUTTONDOWN, pg.JOYHATMOTION, pg.JOYAXISMOTION):
-                logger.debug(f"JOY {event.instance_id}: {event.__dict__.get("button", "")} "
-                             f"{event.__dict__.get("axis", "")} {event.__dict__.get("hat", "")} "
-                             f"{event.__dict__.get("value", "")}")
+                logger.info(f"Mapping: {c.get_mapping()}")
 
             if event.type == pg.CONTROLLERAXISMOTION:
-                logger.info(f"Axis {event.axis}: {event.value}")
+                logger.debug(f"Axis {AXIS_NAMES[event.axis]}: {event.value}")
             if event.type == pg.CONTROLLERBUTTONDOWN:
-                logger.info(f"Button {event.button} down")
+                logger.debug(f"Button {BUTTON_NAMES[event.button]} down")
             if event.type == pg.CONTROLLERBUTTONUP:
-                logger.info(f"Button {event.button} up")
+                logger.debug(f"Button {BUTTON_NAMES[event.button]} up")
 
         screen.fill((0, 0, 0))
 
-        lines = []
-        for c in controllers.values():
-            lines.append(f"Controller {c.id} {c.name}")
+        lines: list[str] = [f"{pgc.get_count()} controllers detected."]
+
+        for i, c in enumerate(controllers.values()):
+            if display_index != i:
+                continue
+            lines.append(f"Controller {display_index + 1}/{pgc.get_count()}")
+            lines.append(f"Controller ID-{c.id} \"{c.name}\"")
             lines.append(f"Rumble supported: {rumbles[c.id]}")
             lines.append(f"Attached: {bool(c.attached())}")
 
-            for axis in (pg.CONTROLLER_AXIS_LEFTX, pg.CONTROLLER_AXIS_LEFTY, pg.CONTROLLER_AXIS_RIGHTX,
-                         pg.CONTROLLER_AXIS_RIGHTY, pg.CONTROLLER_AXIS_TRIGGERLEFT, pg.CONTROLLER_AXIS_TRIGGERRIGHT):
-                lines.append(f"Axis {axis}: {c.get_axis(axis)}")
+            for axis, name in AXIS_NAMES.items():
+                lines.append(f"Axis {name}: {c.get_axis(axis)}")
 
-            for button in (pg.CONTROLLER_BUTTON_A, pg.CONTROLLER_BUTTON_B, pg.CONTROLLER_BUTTON_X,
-                           pg.CONTROLLER_BUTTON_Y, pg.CONTROLLER_BUTTON_DPAD_UP, pg.CONTROLLER_BUTTON_DPAD_DOWN,
-                           pg.CONTROLLER_BUTTON_DPAD_LEFT, pg.CONTROLLER_BUTTON_DPAD_RIGHT,
-                           pg.CONTROLLER_BUTTON_LEFTSHOULDER, pg.CONTROLLER_BUTTON_RIGHTSHOULDER,
-                           pg.CONTROLLER_BUTTON_LEFTSTICK, pg.CONTROLLER_BUTTON_RIGHTSTICK, pg.CONTROLLER_BUTTON_BACK,
-                           pg.CONTROLLER_BUTTON_START, pg.CONTROLLER_BUTTON_GUIDE):
-                lines.append(f"Button {button}: {c.get_button(button)}")
+            for button, name in BUTTON_NAMES.items():
+                lines.append(f"Button {name}: {c.get_button(button)}")
 
         screen.blit(
-            font.render("\n".join(lines), True, "white", "black", 500 - 20), (10, 10)
+            font.render("\n".join(lines), True, "white", "black", 500 - 20), (10, 5)
         )
 
         pg.display.flip()
