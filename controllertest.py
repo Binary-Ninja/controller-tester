2eef76f9d62b82e059270b3aa54ffc5ee8912313
Binary-Ninja <91568729+Binary-Ninja@users.noreply.github.com>
2024-04-30 13:00:47 -0600

    Add files via upload

--- controllertest.py
+++ controllertest.py
@@ -7,17 +7,18 @@ import pygame as pg
 import pygame._sdl2.controller as pgc
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename="controllertest.log", filemode="w", level=logging.DEBUG)
+logging.basicConfig(filename="controllertest.log", filemode="w", level=logging.INFO)
 
 # Must be set before initializing modules.
 os.environ["SDL_JOYSTICK_ALLOW_BACKGROUND_EVENTS"] = "1"
 
 
 def add_mappings(filename: str):
-    """Set the gamepad mappings config file to the given filename."""
+    """Add controller mappings from the given filename."""
     os.environ["SDL_GAMECONTROLLERCONFIG_FILE"] = os.path.join(os.getcwd(), filename)
 
 
+# Must be set before initializing modules.
 add_mappings("gamecontrollerdb.txt")
 
 pg.init()
@@ -61,6 +62,7 @@ def main():
     clock = pg.time.Clock()
     font = pg.Font(None, 25)
 
+    pending_controllers: list[pgc.Controller] = []
     controllers: dict[int, pgc.Controller] = {}
     rumbles: dict[int, bool] = {}
     display_index: int = 0
@@ -83,110 +85,123 @@ def main():
                     display_index -= 1
                     display_index %= len(controllers)
                 if event.key == pg.K_r:
-                    for c in controllers.values():
-                        rumbles[c.id] = c.rumble(0.7, 0.7, 500)
-                        logger.info(f"Rumble test Controller ID-{c.id} \"{c.name}\": {rumbles[c.id]}")
+                    for iid, c in controllers.items():
+                        rumbles[iid] = c.rumble(0.7, 0.7, 500)
+                        logger.info(f"Rumble test Controller DI-{c.id} II-{iid} \"{c.name}\": {rumbles[iid]}")
 
             if event.type == pg.JOYDEVICEADDED:
                 j = pg.Joystick(event.device_index)
+                iid = j.get_instance_id()
+                name = j.get_name()
                 if pgc.is_controller(event.device_index):
-                    logger.debug(f"Joystick ID-{event.device_index} \"{j.get_name()}\" added (Controller).")
+                    logger.debug(f"Joystick DI-{event.device_index} II-{iid} \"{name}\" added (Controller).")
                     logger.debug(f"GUID: {j.get_guid()}")
                 else:
-                    logger.info(f"Joystick ID-{event.device_index} \"{j.get_name()}\" added (Joystick).")
+                    logger.info(f"Joystick DI-{event.device_index} II-{iid} \"{name}\" added (Joystick).")
                     logger.info(f"GUID: {j.get_guid()}")
                     logger.info("Attempting to convert bad Joystick...")
                     try:
                         c = pgc.Controller.from_joystick(pg.Joystick(event.device_index))
                         rumble = c.rumble(0.7, 0.7, 500)
-                        logger.info(f"Converted ID-{c.id} \"{c.name}\". Rumble: {rumble}")
+                        logger.info(f"Converted DI-{c.id} \"{c.name}\". Rumble: {rumble}")
                         logger.info(f"Mapping: {c.get_mapping()}")
                     except Exception as ex:
-                        logger.warning(f"Failed to convert Joystick ID-{event.device_index} \"{j.get_name()}\"")
+                        logger.warning(f"Failed to convert Joystick DI-{event.device_index} II-{iid} \"{name}\"")
                         logger.warning(f"{ex.__class__.__name__}: {ex}")
 
             if event.type == pg.JOYDEVICEREMOVED:
-                logger.debug(f"Joystick ID-{event.instance_id} removed.")
+                logger.debug(f"Joystick II-{event.instance_id} removed.")
             if event.type == pg.JOYAXISMOTION:
-                logger.debug(f"Joystick ID-{event.instance_id} Axis {event.axis}: {event.value}")
+                logger.debug(f"Joystick II-{event.instance_id} Axis {event.axis}: {event.value}")
             if event.type == pg.JOYHATMOTION:
-                logger.debug(f"Joystick ID-{event.instance_id} Hat {event.hat}: {event.value}")
+                logger.debug(f"Joystick II-{event.instance_id} Hat {event.hat}: {event.value}")
             if event.type == pg.JOYBALLMOTION:
-                logger.debug(f"Joystick ID-{event.instance_id} Ball {event.ball}: {event.rel}")
+                logger.debug(f"Joystick II-{event.instance_id} Ball {event.ball}: {event.rel}")
             if event.type == pg.JOYBUTTONDOWN:
-                logger.debug(f"Joystick ID-{event.instance_id} Button {event.button}: down")
+                logger.debug(f"Joystick II-{event.instance_id} Button {event.button}: down")
             if event.type == pg.JOYBUTTONUP:
-                logger.debug(f"Joystick ID-{event.instance_id} Button {event.button}: up")
+                logger.debug(f"Joystick II-{event.instance_id} Button {event.button}: up")
 
             if event.type == pg.CONTROLLERDEVICEADDED:
                 try:
                     c = pgc.Controller(event.device_index)
                 except Exception as ex:
-                    logger.warning(f"Controller ID-{event.device_index} could not be initialized.")
+                    logger.warning(f"Controller DI-{event.device_index} could not be initialized.")
+                    logger.warning(f"is_controller: {pgc.is_controller(event.device_index)}")
                     logger.warning(f"GUID: {event.guid}")
                     logger.warning(f"{ex.__class__.__name__}: {ex}")
                     continue
-                controllers[c.id] = c
-                rumbles[c.id] = c.rumble(0.7, 0.7, 500)
-                logger.info(f"Controller ID-{c.id} \"{c.name}\" added. Rumble: {rumbles[c.id]}")
+                logger.info(f"Controller DI-{c.id} \"{c.name}\" found.")
+                logger.info(f"is_controller: {pgc.is_controller(event.device_index)}")
                 logger.info(f"GUID: {event.guid}")
-                logger.info(f"Mapping: {c.get_mapping()}")
-                # logger.info(f"crc: {c.get_mapping()["crc"]}")
+
+                pending_controllers.append(c)
+                c.set_mapping(c.get_mapping())  # Trigger a pygame.CONTROLLERDEVICEREMAPPED event.
 
             if event.type == pg.CONTROLLERDEVICEREMOVED:
-                if event.instance_id in controllers:
-                    c = controllers[event.instance_id]
-                    logger.info(f"Controller ID-{c.id} \"{c.name}\" removed.")
-                    del controllers[c.id]
-                    del rumbles[c.id]
+                iid = event.instance_id
+                if iid in controllers:
+                    c = controllers[iid]
+                    logger.info(f"Controller DI-{c.id} II-{iid} \"{c.name}\" removed.")
+                    del controllers[iid]
+                    del rumbles[iid]
                     if len(controllers):
                         display_index %= len(controllers)
                     else:
                         display_index = 0
                 else:
-                    logger.info("Controller ID-?? \"UNKNOWN\" removed.")
+                    logger.info(f"Controller DI-?? II-{iid} \"UNKNOWN\" removed.")
 
             if event.type == pg.CONTROLLERDEVICEREMAPPED:
-                if event.instance_id in controllers:
-                    c = controllers[event.instance_id]
-                    logger.info(f"Controller ID-{c.id} \"{c.name}\" remapped.")
+                iid = event.instance_id
+                if iid in controllers:
+                    c = controllers[iid]
+                    logger.info(f"Controller DI-{c.id} II-{iid} \"{c.name}\" remapped.")
                     logger.info(f"Mapping: {c.get_mapping()}")
                 else:
-                    logger.info("Controller ID-?? \"UNKNOWN\" remapped.")
+                    if pending_controllers:
+                        c = pending_controllers.pop(0)
+                        controllers[iid] = c
+                        rumbles[iid] = c.rumble(0.7, 0.7, 500)
+                        logger.info(f"Controller DI-{c.id} II-{iid} \"{c.name}\" added. Rumble: {rumbles[iid]}")
+                        logger.info(f"Mapping: {c.get_mapping()}")
+                    else:
+                        logger.info(f"Controller DI-?? II-{iid} \"UNKNOWN\" remapped.")
 
             if event.type == pg.CONTROLLERAXISMOTION:
                 if event.instance_id in controllers:
                     c = controllers[event.instance_id]
-                    num, name = c.id, c.name
+                    num, num2, name = c.id, event.instance_id, c.name
                 else:
-                    num, name = "??", "UNKNOWN"
-                logger.debug(f"Controller ID-{num} \"{name}\" Axis {AXIS_NAMES[event.axis]}: {event.value}")
+                    num, num2, name = "??", event.instance_id, "UNKNOWN"
+                logger.debug(f"Controller DI-{num} II-{num2} \"{name}\" Axis {AXIS_NAMES[event.axis]}: {event.value}")
             if event.type == pg.CONTROLLERBUTTONDOWN:
                 if event.instance_id in controllers:
                     c = controllers[event.instance_id]
-                    num, name = c.id, c.name
+                    num, num2, name = c.id, event.instance_id, c.name
                 else:
-                    num, name = "??", "UNKNOWN"
-                logger.debug(f"Controller ID-{num} \"{name}\" Button {BUTTON_NAMES[event.button]} down")
+                    num, num2, name = "??", event.instance_id, "UNKNOWN"
+                logger.debug(f"Controller DI-{num} II-{num2} \"{name}\" Button {BUTTON_NAMES[event.button]} down")
             if event.type == pg.CONTROLLERBUTTONUP:
                 if event.instance_id in controllers:
                     c = controllers[event.instance_id]
-                    num, name = c.id, c.name
+                    num, num2, name = c.id, event.instance_id, c.name
                 else:
-                    num, name = "??", "UNKNOWN"
-                logger.debug(f"Controller ID-{num} \"{name}\" Button {BUTTON_NAMES[event.button]} up")
+                    num, num2, name = "??", event.instance_id, "UNKNOWN"
+                logger.debug(f"Controller DI-{num} II-{num2} \"{name}\" Button {BUTTON_NAMES[event.button]} up")
 
         screen.fill((0, 0, 0))
 
         lines: list[str] = [f"{pgc.get_count()} controller(s) detected. (ESC to quit program.)"]
 
-        for i, c in enumerate(controllers.values()):
+        for i, tup in enumerate(controllers.items()):
+            iid, c = tup
             if display_index != i:
                 continue
 
             lines.append(f"Controller {display_index + 1}/{pgc.get_count()} (Use arrow keys to cycle controllers.)")
-            lines.append(f"Controller ID-{c.id} \"{c.name}\"")
-            lines.append(f"Rumble supported: {rumbles[c.id]} (Press R to test rumble.)")
+            lines.append(f"Controller DI-{c.id} II-{iid} \"{c.name}\"")
+            lines.append(f"Rumble supported: {rumbles[iid]} (Press R to test rumble.)")
             lines.append(f"Attached: {bool(c.attached())}")
 
             for axis, name in AXIS_NAMES.items():
